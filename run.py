283c95c7574f591bb805e2d2d798e5ead5bd8230
Djamel Nafisse <djamel.nafisse@efrei.net>
2025-07-08 16:56:51 +0200

    up

--- run.py
+++ run.py
@@ -8,6 +8,5 @@ def handle_connect():
     print("Client connecté")
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 10000))
-    #socketio.run(app, host="0.0.0.0", port=port)
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
