493c4412445af83691f8c3f560e4327823c59825
Djamel Nafisse <djamel.nafisse@efrei.net>
2025-07-08 13:42:21 +0200

    update

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from extensions import socketio
 from app import create_app
-
+import os
 app = create_app()
 
 @socketio.on('connect')
@@ -8,4 +8,5 @@ def handle_connect():
     print("Client connecté")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
