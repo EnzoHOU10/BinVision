0bd56820f7f62f97a04dc984a39e6fcda34d4e9f
Djamel Nafisse <djamel.nafisse@efrei.net>
2025-07-08 15:33:08 +0200

    update

--- extensions.py
+++ extensions.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from sqlalchemy import create_engine
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 
-db = SQLAlchemy()
+engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+db = SQLAlchemy(session_options={"bind": engine})
 socketio = SocketIO()
